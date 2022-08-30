be5153cf0fb149a57af1916eee86f24268a10fcd
AnthonyL03 <lilopez0803@gmail.com>
2022-08-30 03:33:35 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,17 @@
+/**
+ * Author: Anthony Lopez
+ *         alopez55@huskers.unl.edu
+ * Date: 2022/08/23
+ *
+ * A simple hello world program in C
+ *
+ */
+#include <stdlib.h>
+#include <stdio.h>
+
+int main(int argc, char **argv) {
+
+  printf("Anthony Lopez, Electrical Engineer\n");
+
+  return 0;
+}
